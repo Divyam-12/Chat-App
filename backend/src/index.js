83977ccefbdcc29d34693872dce96aef99e57b8f
Divyam-12 <patnidivyam@gmail.com>
2025-08-22 00:35:56 +0530

    frontend URL changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const PORT=process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-app-1-icru.onrender.com',
     credentials: true,
 }));
 
